71f02efd7aa33b3c89387e75741b6a676ecfc20b
Anderson Ochoa <andersonochoay@gmail.com>
2025-12-03 18:02:03 -0500

    feat: Initial functionality added
    -Basic HTML and CSS added.
    -User can enter the grid size he would like to.

---
+++ script.js
@@ -0,0 +1,35 @@
+const drawContainer = document.getElementById("container")
+const button = document.querySelector("button")
+
+
+function createGrid(size) {
+    const container = document.getElementById("container");
+    container.innerHTML = ""; 
+
+    const squareSize = 480 / size; 
+
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+        div.style.width = squareSize + "px";
+        div.style.height = squareSize + "px";
+        div.style.boxSizing = "border-box";
+
+        div.addEventListener("mouseover", () => {div.style.backgroundColor = "black"})
+
+        container.appendChild(div);
+    }
+}
+
+
+button.addEventListener("click", () => {
+    const squares = parseInt(prompt("Enter the grid size (1-100): "));
+
+    if(squares > 0 && squares <= 100){
+        createGrid(squares)
+    } else {
+        alert("Please enter a number between 1 and 100.")
+    }
+})
+
+createGrid(16);
+
